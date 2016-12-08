efae0e34e6561efe8090b7c39814b3020e663378
Nikolay Ryabov <pyatyispyatil@gmail.com>
2016-12-08 18:49:02 +0300

    add comments

--- src/stepper.js
+++ src/stepper.js
@@ -1,7 +1,7 @@
 class StepDescriptor {
   /**
-   * @param {Function} step - действие, которое будет выполнено при выполнении данного шага
-   * @param {Stepper} stepper - инстанс класса Stepper, который осуществляет управление шагами
+   * @param {Function} step - action, which will be carried out in the executing of this step
+   * @param {Stepper} stepper - instance of Stepper, which contains this StepDescriptor
    * */
   constructor(step, stepper) {
     this.id = StepDescriptor.ID_COUNTER;
@@ -14,17 +14,24 @@ class StepDescriptor {
 
   id = 0;
   action;
+  stepper;
 
   static ID_COUNTER = 0;
 
+  /**
+   * @param {*} data
+   * */
   next(data) {
-    this.stepper.next(data, this);
+    return this.stepper.next(data, this);
   }
 
   remove() {
     this.stepper.remove(this);
   }
 
+  /**
+   * @param {*} data
+   * */
   reject(data) {
     this.stepper.reject(data);
   }
@@ -32,8 +39,8 @@ class StepDescriptor {
 
 export class Stepper {
   /**
-   * @param {Function[]} steps - массив шагов, которые будут обрабатываться
-   * @param {Function} onReject - колбэк, который сработает при выбросом исключения одним из шагов
+   * @param {Function[]} steps - array of steps, which will be treated
+   * @param {Function} onReject - callback, which will be executing on some step
    * */
   constructor(steps, onReject = () => null) {
     steps.forEach((step) => this.add(step));
@@ -45,6 +52,11 @@ export class Stepper {
 
   currentStep = -1;
 
+  /**
+   * @param {*} [data]
+   * @param {StepDescriptor} [stepDescriptor]
+   * @return {Boolean} flag of the last step
+   * */
   next(data = null, stepDescriptor = null) {
     if (stepDescriptor) {
       this.currentStep = this.steps.findIndex((step) => step.id === stepDescriptor.id);
@@ -53,19 +65,25 @@ export class Stepper {
     this.currentStep++;
     this.steps[this.currentStep].execute(data);
 
-    return {
-      done: this.currentStep === this.steps.length - 1
-    }
+    return this.currentStep === this.steps.length - 1;
   }
 
   prev() {
     this.currentStep--;
   }
 
+  /**
+   * @param {StepDescriptor} stepDescriptor
+   * */
   remove(stepDescriptor) {
     this.steps.splice(this.steps.findIndex((step) => step.id === stepDescriptor.id), 1);
   }
 
+  /**
+   * @param {Function} step
+   * @param {Number} index
+   * @return {StepDescriptor}
+   * */
   add(step, index = null) {
     const stepDescriptor = new StepDescriptor(step, this);
 
@@ -78,14 +96,43 @@ export class Stepper {
     return stepDescriptor;
   }
 
+  /**
+   * @param {StepDescriptor} stepDescriptor
+   * @return {Number}
+   * */
+  getIndex(stepDescriptor) {
+    let index = this.steps.findIndex((step) => step.id === stepDescriptor.id);
+
+    if (index === -1) {
+      throw new Error('Cannot find step in steps array');
+    } else {
+      return index;
+    }
+  }
+
+  /**
+   * @param {StepDescriptor} stepDescriptor - descriptor of the step before which will be inserted a new step
+   * @param {Function} step - callback for the new step descriptor
+   * @return {StepDescriptor}
+   * */
   insertBefort(stepDescriptor, step) {
-    return this.add(step, this.steps.findIndex((step) => step.id === stepDescriptor.id) - 1);
+    return this.add(step, this.getIndex(stepDescriptor) - 1);
   }
 
+  /**
+   * @param {StepDescriptor} stepDescriptor - descriptor of the step after which will be inserted a new step
+   * @param {Function} step - callback for the new step descriptor
+   * @return {StepDescriptor}
+   * */
   insertAfter(stepDescriptor, step) {
-    return this.add(step, this.steps.findIndex((step) => step.id === stepDescriptor.id) + 1);
+    return this.add(step, this.getIndex(stepDescriptor) + 1);
   }
 
+  /**
+   * Treats steps and return a sequence of all steps. It can not be edited.
+   * In every step arguments will be a Object with "next" and "reject" methods.
+   * @return {Function} first step
+   * */
   sequence() {
     return sequence(this.steps.map(step => step.raw), this.reject);
   }
